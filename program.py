7ec4f8ab08c15f1916259f661a0d65dad55b1e93
DRsalt450 <grivalsky.simon@norskakladno.cz>
2025-03-13 16:29:32 +0100

    rap.

--- program.py
+++ program.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>i dont have drugs in my left shoe.</p>"
+
+@app.route("/page")
+def page():
+    return "<p>yo im a rapper, more like a wrapper. i eat banana, you eat canada</p>"
